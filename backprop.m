68bede225b72a7fb2fe3cf56928c4cc1327243b9
Peter Stanton <peterstanton@users.noreply.github.com>
2017-11-11 18:34:17 -0800

    Add files via upload

--- backprop.m
+++ backprop.m
@@ -4,7 +4,7 @@ function output = backprop()
  in1 = [0,0,0,0,0,0,1,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0];
  in2 = [1,0,0,0,0,0,1,0,0,1,1,1,1,0,0,1,0,1,0,1,1,0,0,1,0,0,0,0,0,1];
  w   = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
- b   = 0;
+ 
  
  HidLayerNeurons = 6;  % Number of hidden layer neurons
  
@@ -18,14 +18,29 @@ function output = backprop()
 HiddenLayerWeights = rand(3,3); % Weight matrix from Input to Hidden
  
 OutputLayerWeights = rand(HidLayerNeurons,3); % Weight matrix from Hidden to Output
- 
+
+biasHidden = rand(HidLayerNeurons,3);         % Random bias.
  
 IterationCount = 0;  %Count passes.
 
+error = 1.00;
 
-
-function trainNet(Input,Output,HiddenLayerWeights,OutputLayerWeights)
- 
- 
-
-
+while (error > 0.05)
+    IterationCount = IterationCount + 1;                %Increment the counter
+    Error_Mat(IterationCount)=error;                    %Put error rate in matrix.
+    outOfHidden = Input;                                %start computing output
+    outOfHidden = HiddenLayerWeights' * outOfHidden;    %Mult by weight matrix.
+    outOfHidden = outOfHidden + biasHidden;             %Add bias
+    outOfHidden = logsig(outOfHidden);                  %Apply log sigmoid function
+    
+    
+    outOfOutput = outOfHidden;
+    outOfOutput = OutputLayerWeights' * outOfOutput;
+    outOfOutput = outOfOutput + biasOutput;
+    outOfOutput = logsig(outOfOutput);
+    
+    myError = Output - outOfOutput;
+    
+    S2 = -2 * diff(logsig(n^2 * e
+    
+        

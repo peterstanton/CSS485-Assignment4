4818fa03fbbd6936d9709c0e54fa85005100e4d6
Peter Stanton <peterstanton@users.noreply.github.com>
2017-11-14 22:03:26 -0800

    Add files via upload

--- backprop.m
+++ backprop.m
@@ -4,7 +4,7 @@ function output = backprop()
  in1 = [0,0,0,0,0,0,1,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0];
  in2 = [1,0,0,0,0,0,1,0,0,1,1,1,1,0,0,1,0,1,0,1,1,0,0,1,0,0,0,0,0,1];
  
- LearningRate = 0.1;
+ LearningRate = 0.05;
   
  t0 = [1,0,0];
  t1 = [0,1,0];
@@ -13,21 +13,25 @@ function output = backprop()
  Input = [in0; in1; in2]';
  Output = [t0; t1; t2]';
  
- NumHidLayerNeurons = 5;
+ NumHidLayerNeurons = 3;
  
 HiddenLayerWeights = rand(NumHidLayerNeurons,30); % Weight matrix from Input to Hidden
 OutputLayerWeights = rand(3,NumHidLayerNeurons); % Weight matrix from Hidden to Output
 biasHidden = rand(NumHidLayerNeurons,1);         % Random bias.
 biasOutput = rand(3,1);
- 
+
 IterationCount = 0;  %Count passes.
 ErrorVec1(1:1000000) = 0;
 xVec(1:1000000) = 0;
 tester = 10.00;
-
+i = 0;
 %while (tester > 0.05)
 while(IterationCount < 10000)
-    i = randi(3);
+    if i == 3
+        i = 1;
+    else
+        i = i + 1;
+    end
     IterationCount = IterationCount + 1;           %Increment the counter
     outOfHidden = logsig(HiddenLayerWeights * Input(:,i) + biasHidden);   
     outOfOutput = logsig(OutputLayerWeights * outOfHidden + biasOutput);
@@ -50,25 +54,28 @@ end
 
 
 IterationCount = 0;  %Count passes.
-ErrorVec0(1:1000000) = 0;
+ErrorVec0(1:5000) = 0;
 xVec(1:1000000) = 0;
 tester = 10.00;
-
+i = 0;
 %while (tester > 0.05)
-while(IterationCount < 10000)
-    i = randi(3);
+while(IterationCount < 5000)
+    if i == 3
+        i = 1;
+    else
+        i = i + 1;
+    end
     IterationCount = IterationCount + 1;           %Increment the counter
     outOfHidden = logsig(HiddenLayerWeights * Input(:,i) + biasHidden);   
     outOfOutput = logsig(OutputLayerWeights * outOfHidden + biasOutput);
     
    myError = Output(:,i) - outOfOutput;
+
    
    xVec(IterationCount) = IterationCount;
    ErrorVec0(IterationCount) = sum(myError.^2)/length(myError);
    tester = sum(myError.^2);    
 end
-
-%E = [ErrorVec(1:end-3) + ErrorVec(2:end-2) + ErrorVec(3:end-1)]/3;
 
 corrupt1 = randi([1 30]);
 corrupt2 = randi([1 30]);
@@ -83,14 +90,18 @@ for i = 1:3
 end
 
 
-ErrorVec2(1:1000000) = 0;
+ErrorVec2(1:5000) = 0;
 IterationCount = 0;
 tester = 10.0;
 myError = 0;
-
+i = 0;
 %while (tester > 0.05)
 while(IterationCount < 5000)
-    i = randi(3);
+    if i == 3
+        i = 1;
+    else
+        i = i + 1;
+    end
     IterationCount = IterationCount + 1;           %Increment the counter
     outOfHidden = logsig(HiddenLayerWeights * Input(:,i) + biasHidden);   
     outOfOutput = logsig(OutputLayerWeights * outOfHidden + biasOutput);
@@ -108,10 +119,10 @@ corrupt2 = randi([1 30]);
 corrupt3 = randi([1 30]);
 corrupt4 = randi([1 30]);
 
-corrupt5 = randi([1 30]);
-corrupt6 = randi([1 30]);
-corrupt7 = randi([1 30]);
-corrupt8 = randi([1 30]);
+% corrupt5 = randi([1 30]);
+% corrupt6 = randi([1 30]);
+% corrupt7 = randi([1 30]);
+% corrupt8 = randi([1 30]);
 
 
 for i = 1:3
@@ -119,20 +130,24 @@ for i = 1:3
     Input(corrupt2,i) = ~Input(corrupt2,i);
     Input(corrupt3,i) = ~Input(corrupt3,i);
     Input(corrupt4,i) = ~Input(corrupt4,i);
-    Input(corrupt5,i) = ~Input(corrupt5,i);
-    Input(corrupt6,i) = ~Input(corrupt6,i);
-    Input(corrupt7,i) = ~Input(corrupt7,i);
-    Input(corrupt8,i) = ~Input(corrupt8,i);
+%     Input(corrupt5,i) = ~Input(corrupt5,i);
+%     Input(corrupt6,i) = ~Input(corrupt6,i);
+%     Input(corrupt7,i) = ~Input(corrupt7,i);
+%     Input(corrupt8,i) = ~Input(corrupt8,i);
 end
 
 
-ErrorVec3(1:1000000) = 0;
+ErrorVec3(1:5000) = 0;
 IterationCount = 0;
 tester = 10.0;
 myError = 0;
-
+i = 0;
 while(IterationCount < 5000)
-    i = randi(3);
+    if i == 3
+        i = 1;
+    else
+        i = i + 1;
+    end
     IterationCount = IterationCount + 1;           %Increment the counter
     outOfHidden = logsig(HiddenLayerWeights * Input(:,i) + biasHidden);   
     outOfOutput = logsig(OutputLayerWeights * outOfHidden + biasOutput);
@@ -153,6 +168,7 @@ ylabel('Squared Error')
 
 graphx = 0:4:8;
 graphy = [mean(ErrorVec0) mean(ErrorVec2) mean(ErrorVec3)];
+
 figure(2)
 bar(graphx,graphy);
 title('Mean Error for Backpropagation Learning Digit Recognition')
